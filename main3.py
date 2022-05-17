7047b629e81cc7bc8ab0b44b26b75b6d573cdb1c
fdv1 <francois@pytek.io>
2022-05-17 21:57:08 +0100

    added tabs

--- main3.py
+++ main3.py
@@ -1,10 +1,20 @@
-from reflect_antd import Button, InputNumber, Modal, Table, Checkbox, Col
+from reflect_antd import (
+    Button,
+    InputNumber,
+    Modal,
+    Table,
+    Checkbox,
+    Col,
+    Radio,
+    Tabs,
+    pagination,
+)
 from reflect_html import div
 from itertools import count, chain
 from reflect import make_observable
 from reflect_utils.antd import create_form_row
 
-columns = [
+COLUMNS_DONNES = [
     {"title": "Donne", "dataIndex": "key"},
     {"title": "Prise", "dataIndex": "prise"},
     {"title": "Bouts", "dataIndex": "bouts"},
@@ -12,6 +22,13 @@ columns = [
     {"title": "Defense", "dataIndex": "pts_defense"},
     {"title": "", "dataIndex": "edit"},
 ]
+
+COLUMNS_PLAYERS = [
+    {"title": "Nom", "dataIndex": "key"},
+    {"title": "Points", "dataIndex": "points"},
+]
+
+TITLE = "Tarot"
 
 
 class Donne:
@@ -31,6 +48,12 @@ class Donne:
         )
 
 
+class Player:
+    def __init__(self):
+        self.key = "test"
+        self.points = 0
+
+
 def update_dict(d, **kwargs):
     return dict(chain(d.items(), kwargs.items()))
 
@@ -41,6 +64,7 @@ class App:
         self.show_edit_donne = make_observable(False, key="show_edit_donne")
         self.edit_donne_observable = make_observable(Donne(0), key="show_edit_donne")
         self.donne_counter = count(1)
+        self.players = make_observable([Player()], depth=1)
 
     def create_callback(self, donne):
         def callback():
@@ -49,13 +73,16 @@ class App:
 
         return callback
 
-    def generate_rows(self):
+    def generate_rows_donne(self):
         return [
             update_dict(
                 donne.to_dict(), edit=Button("...", onClick=self.create_callback(donne))
             )
             for donne in self.donnes()
         ] + [{"edit": Button("+", onClick=self.on_add), "key": ""}]
+
+    def generate_rows_players(self):
+        return [player.__dict__ for player in self.players()]
 
     def hide_panel(self):
         self.show_edit_donne.set(False)
@@ -74,6 +101,15 @@ class App:
         self.show_panel()
 
     def content(self):
+        prise = Radio.Group(
+            [
+                Radio("Petite", value="petite"),
+                Radio("Garde", value="garde"),
+                Radio("Garde contre", value="garde_contre"),
+                Radio("Garde sans", value="garde_sans"),
+            ],
+            value=self.edit_donne_observable.prise,
+        )
         compute_other_score = lambda s: 91 - s if s else None
         pts_preneur = InputNumber(
             value=self.edit_donne_observable.pts_preneur,
@@ -94,22 +130,48 @@ class App:
             value=self.edit_donne_observable.bouts,
             style=dict(marginLeft=28, marginTop=10),
         )
-        return div(
+        return Tabs(
             [
-                Table(columns=columns, dataSource=self.generate_rows, pagination=False),
-                Modal(
-                    Col(
+                Tabs.TabPane(
+                    div(
                         [
-                            create_form_row("Preneur", pts_preneur),
-                            create_form_row("Defense", pts_defense),
-                            create_form_row("Bouts", bouts),
+                            Table(
+                                columns=COLUMNS_DONNES,
+                                dataSource=self.generate_rows_donne,
+                                pagination=False,
+                            ),
+                            Modal(
+                                Col(
+                                    [
+                                        create_form_row("Prise", prise),
+                                        create_form_row("Preneur", pts_preneur),
+                                        create_form_row("Defense", pts_defense),
+                                        create_form_row("Bouts", bouts),
+                                    ]
+                                ),
+                                title=lambda: f"Donne {self.edit_donne_observable.key()}",
+                                visible=self.show_edit_donne,
+                                onOk=self.on_ok,
+                                onCancel=self.hide_panel,
+                                closable=True,
+                            ),
                         ]
                     ),
-                    title=lambda: f"Donne {self.edit_donne_observable.key()}",
-                    visible=self.show_edit_donne,
-                    onOk=self.on_ok,
-                    onCancel=self.hide_panel,
-                    closable=True,
+                    key="Donnes",
+                    tab="Donnes",
+                ),
+                Tabs.TabPane(
+                    div(
+                        [
+                            Table(
+                                columns=COLUMNS_PLAYERS,
+                                dataSource=self.generate_rows_players,
+                                pagination=False,
+                            )
+                        ]
+                    ),
+                    key="Joueurs",
+                    tab="Joueurs",
                 ),
             ]
         )
